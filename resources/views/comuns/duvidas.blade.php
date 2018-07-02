8e1263f70acc713887a48b37d79164c80b5842e8
bunicula <36703368+bunicula@users.noreply.github.com>
2018-07-02 12:52:57 -0300

    duvidas

---
+++ resources/views/comuns/duvidas.blade.php
@@ -0,0 +1,19 @@
+@extends('comuns.estatico.layout')
+@section('conteudo')
+
+<div class="container">
+    <br></br>
+      <h5 class="text h4 mb-2 font-weight-normal" style="color: #0d823b;">Como realizar o orçamento?</h5>
+      <p class="text" align="justify">Para realizar um orçamento, vá a página de produtos. Nela há o botão 'Gostou dos nossos produtos?' no lado direito da página. Ao clicar nele, você será redirecionado para a página de orçamentos. </p>
+      <br><h5 class="text h4 mb-2 font-weight-normal" style="color: #0d823b;">Por que minha localização é necessária?</h5>
+      <p class="text">O Sistema precisa da sua localização para encontrar o distribuidor mais próximo a você, dessa forma, a entrega do produto é otimizada.</p>
+      <br><h5 class="text h4 mb-2 font-weight-normal" style="color: #0d823b;">Quais as formas de pagamento?</h5>
+      <p class="text">É necessário conversar com o seu distribuidor para saber qual a forma de pagamento será utilizada.</p>
+      <br><h5 class="text h4 mb-2 font-weight-normal" style="color: #0d823b;">Prazo de entrega de um produto</h5>
+      <p class="text">Os prazos de entrega variam conforme a região. Normalmente, o limite máximo é de 10 dias. </p>
+    </div>
+    <br></br>
+    <br></br>
+
+  </div>
+@endsection
