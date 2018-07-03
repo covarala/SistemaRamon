0ef853e43d106e507405fd9ee9b9813d10b849b3
Lucasguimaraesp <Lucasguimaraesp@users.noreply.github.com>
2018-07-03 14:39:25 -0300

    carrossel atualizado

--- resources/views/comuns/inicial.blade.php
+++ resources/views/comuns/inicial.blade.php
@@ -1,39 +1,43 @@
 @extends('comuns.estatico.layout')
 
 @section('conteudo')
-<!–– Carrossel de imagens : ––>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="{{ asset('imagens\BANNER1.png')}}" width="1260" height="465" alt="First slide">
-        </div>
-        <div class="carousel-item">
-
-      <a href="{{route('produtos')}}">
-       <img class="d-block w-100"  src="{{ asset('imagens\BANNER-2.png')}}" width="1260" height="465" alt="Second slide">
-       </a>
-        </div>
-
-        <div class="carousel-item">
-            <a href="{{route('sobre')}}">
-          <img class="d-block w-100" src="{{ asset('imagens\BANNER-3.png')}}" width="1260" height="465" alt="Third slide"></a>
-          </a>
-        </div>
+<div class="container-fluid">
+  <!–– Carrossel de imagens : ––>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="{{ asset('imagens\BANNER1.png')}}" width="900" height="420" alt="First slide">
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+      <div class="carousel-item">
+
+        <a href="{{route('produtos')}}">
+          <img class="d-block w-100"  src="{{ asset('imagens\BANNER-2.png')}}" width="900" height="420" alt="Second slide">
+        </a>
+      </div>
+
+      <div class="carousel-item">
+        <a href="{{route('sobre')}}">
+          <img class="d-block w-100" src="{{ asset('imagens\BANNER-3.png')}}" width="900" height="420" alt="Third slide"></a>
+        </a>
+      </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+</div>
+
 
 <!–– Cards dos produtos : ––>
 <div class="album py-5 bg-light">
